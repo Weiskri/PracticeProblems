e536c7416a1f7446795b9d8755e8a75ae66120ad
Weiskri <kkweis13@gmail.com>
2018-01-24 10:16:03 -0500

    added homework problem on for loops

--- PracticeProblems/Program.cs
+++ PracticeProblems/Program.cs
@@ -74,46 +74,66 @@ namespace PracticeProblems
 
             // Problem 1: Muffins
             // Greeting message
-            Console.WriteLine("Hello. Please enter your name.");
-            string userInputName = Console.ReadLine();
+            //Console.WriteLine("Hello. Please enter your name.");
+            //string userInputName = Console.ReadLine();
 
-            // You are baking muffins!
-            Console.WriteLine("{0} is baking muffins. {0} needs seven cups of sugar in the muffin batter.", userInputName);
-            Console.WriteLine("How many cups of sugar has {0} put in the muffin batter?", userInputName);
-            string userInputSugarCupsOne = Console.ReadLine();
-            double userInputSugarCups = Convert.ToDouble(userInputSugarCupsOne);
-            // Tells the user how many more cups of sugar to put in the muffin batter. 
-            double cupsofSugarNeeded = 7 - userInputSugarCups;
-            Console.WriteLine(userInputName + " " + "needs to put" + " " + cupsofSugarNeeded+ " " + "more cups of sugar in the muffin batter.");
+            //// You are baking muffins!
+            //Console.WriteLine("{0} is baking muffins. {0} needs seven cups of sugar in the muffin batter.", userInputName);
+            //Console.WriteLine("How many cups of sugar has {0} put in the muffin batter?", userInputName);
+            //string userInputSugarCupsOne = Console.ReadLine();
+            //double userInputSugarCups = Convert.ToDouble(userInputSugarCupsOne);
+            //// Tells the user how many more cups of sugar to put in the muffin batter. 
+            //double cupsofSugarNeeded = 7 - userInputSugarCups;
+            //Console.WriteLine(userInputName + " " + "needs to put" + " " + cupsofSugarNeeded+ " " + "more cups of sugar in the muffin batter.");
 
-            // Problem 2: Splitting the bill
-            Console.WriteLine("   ");
-            //Inputting Friend Names
-            Console.WriteLine("{0} is out with three friends. Please input {0}'s friends' names. \nWhat is the first name?", userInputName);
-            string friendNameOne = Console.ReadLine();
-            Console.WriteLine("Second name?");
-            string friendNameTwo = Console.ReadLine();
-            Console.WriteLine("And the third friend's name?");
-            string friendNameThree = Console.ReadLine();
-            // Entering the amount each person paid
-            Console.WriteLine("{0}, {1}, {2}, and {3} are eating at a restaurant.", userInputName, friendNameOne, friendNameTwo, friendNameThree);
-            Console.WriteLine("In order to determine the total bill at the restaurant, please enter the amount each person paid.");
-            Console.WriteLine("How much did {0} pay?", userInputName);
-            string personOnePaymentString = Console.ReadLine();
-            Console.WriteLine("How much did {0} pay?", friendNameOne);
-            string personTwoPaymentString = Console.ReadLine();
-            Console.WriteLine("How much did {0} pay?", friendNameTwo);
-            string personThreePaymentString = Console.ReadLine();
-            Console.WriteLine("How much did {0} pay?", friendNameThree);
-            string personFourPaymentString = Console.ReadLine();
-            // Converting these strings to double
-            double personOnePaymentDouble = Convert.ToDouble(personOnePaymentString);
-            double personTwoPaymentDouble = Convert.ToDouble(personTwoPaymentString);
-            double personThreePaymentDouble = Convert.ToDouble(personThreePaymentString);
-            double personFourPaymentDouble = Convert.ToDouble(personFourPaymentString);
+            //// Problem 2: Splitting the bill
+            //Console.WriteLine("   ");
+            ////Inputting Friend Names
+            //Console.WriteLine("{0} is out with three friends. Please input {0}'s friends' names. \nWhat is the first name?", userInputName);
+            //string friendNameOne = Console.ReadLine();
+            //Console.WriteLine("Second name?");
+            //string friendNameTwo = Console.ReadLine();
+            //Console.WriteLine("And the third friend's name?");
+            //string friendNameThree = Console.ReadLine();
+            //// Entering the amount each person paid
+            //Console.WriteLine("{0}, {1}, {2}, and {3} are eating at a restaurant.", userInputName, friendNameOne, friendNameTwo, friendNameThree);
+            //Console.WriteLine("In order to determine the total bill at the restaurant, please enter the amount each person paid.");
+            //Console.WriteLine("How much did {0} pay?", userInputName);
+            //string personOnePaymentString = Console.ReadLine();
+            //Console.WriteLine("How much did {0} pay?", friendNameOne);
+            //string personTwoPaymentString = Console.ReadLine();
+            //Console.WriteLine("How much did {0} pay?", friendNameTwo);
+            //string personThreePaymentString = Console.ReadLine();
+            //Console.WriteLine("How much did {0} pay?", friendNameThree);
+            //string personFourPaymentString = Console.ReadLine();
+            //// Converting these strings to double
+            //double personOnePaymentDouble = Convert.ToDouble(personOnePaymentString);
+            //double personTwoPaymentDouble = Convert.ToDouble(personTwoPaymentString);
+            //double personThreePaymentDouble = Convert.ToDouble(personThreePaymentString);
+            //double personFourPaymentDouble = Convert.ToDouble(personFourPaymentString);
 
-            double totalBill = personOnePaymentDouble + personTwoPaymentDouble + personThreePaymentDouble + personFourPaymentDouble;
-            Console.WriteLine("The total bill is $" + totalBill);
+            //double totalBill = personOnePaymentDouble + personTwoPaymentDouble + personThreePaymentDouble + personFourPaymentDouble;
+            //Console.WriteLine("The total bill is $" + totalBill);
+
+            // homework: create an array with a length of 25 (25 elements), populate that array with a for loop
+
+            // create a for loop, print to the console every other element
+
+            string[] bunnies = new string[25];
+            for (int i =0; i <bunnies.Length; i++)
+            {
+                Console.WriteLine("Enter a breed of rabbit.");
+                bunnies[i] = Console.ReadLine();
+
+            }
+
+            for (int i =0; i <bunnies.Length; i++)
+            {
+                if (i%2 == 0)
+                {
+                    Console.WriteLine(bunnies[i]);
+                }
+            }
 
         
 
